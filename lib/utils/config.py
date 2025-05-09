51b9020ea59ad44769fc0a0f05634bb44bbb5283
Guilherme Zamberlam Pomini <guizamberlam@hotmail.com>
2025-05-09 17:25:58 -0300

    fix: updating config

--- lib/utils/config.py
+++ lib/utils/config.py
@@ -4,13 +4,18 @@ import os
 class ConfigGame:
     def __init__(self):
         self.config = configparser.ConfigParser()
-        config_path = os.path.join(os.getcwd(),'config.cfg')
-        self.config.read(config_path)
+        self.config_path = os.path.join(os.getcwd(),'config.cfg')
+        self.config.read(self.config_path)
     
     def get_muted(self):
         return self.config.getboolean('config.sound', 'muted')
 
     def set_muted(self, value):
         self.config.set('config.sound', 'muted', 'yes' if value else 'no')
+        self.update_config()
+
+    def update_config(self):
+        with open(self.config_path, 'w') as configfile:
+            self.config.write(configfile)
 
 game_config = ConfigGame()
